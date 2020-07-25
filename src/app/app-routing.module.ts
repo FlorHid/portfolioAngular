d300e1938b7fc5c12a3dd385fab20ffa61b0208f
Florencia Hidalgo <blaupunkt_1307@hotmail.com>
2020-07-24 23:26:04 -0300

    first commmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const appRoutes: Routes = [
+    { path: '', component: PortfolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: ItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' }
+
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot( appRoutes )
+    ],
+    exports: [
+        RouterModule
+    ]  
 })
 export class AppRoutingModule { }
